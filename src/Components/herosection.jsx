527fbfeca3e963aaeb997fa0cc42c0e590258f6b
Vinayak Singh <ishita.rs27@gmail.com>
2025-04-09 06:10:54 +0530

    final touch 9-4-25

--- src/Components/herosection.jsx
+++ src/Components/herosection.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "../styles/hero.css";
 import { Link } from "react-router-dom";
+
 const Hero = () => {
   return (
     <div className="hero-container">
+      <div className="hero-bg"></div>
+
       <div id="summersale">
-        <h1>Our Summer Sale is Live </h1>
+        <h1>Our Summer Sale is Live</h1>
         <Link to="/product-ranges">
           <button className="hero-button">Shop Now</button>
         </Link>
